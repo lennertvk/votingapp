2970006ae33c45085935c285dd7d9e43e0294669
Joris Hens <joris@goodbytes.be>
2019-04-04 09:05:12 +0200

    Removed solution
    Signed-off-by: Joris Hens <joris@goodbytes.be>

--- public/javascripts/party.js
+++ public/javascripts/party.js
@@ -2,36 +2,20 @@
 
 class Party {
     constructor() {
+        // set up some basic selectors we'll use often
         let that = this;
         this.audio = document.querySelector("#sndHorn");
         this.image = document.querySelector("#imgHorn");
         this.button = document.querySelector("#btnHorn");
 
-
-        this.primus = Primus.connect("/", {
-            reconnect: {
-                max: Infinity // Number: The max delay before we try to reconnect.
-                    ,
-                min: 500 // Number: The minimum delay before we try reconnect.
-                    ,
-                retries: 10 // Number: How many times we should try to reconnect.
-            }
-        });
-
-        this.primus.on('data', function (data) {
-            if (data.action === "horn") {
-                that.playHorn();
-            }
-        });
-
+        // allow for a click on our button
         this.button.addEventListener("click", function (e) {
-            that.primus.write({
-                'action': 'horn'
-            });
+            alert('clicking');
             e.preventDefault();
         });
     }
 
+    // play the horn!
     playHorn() {
         this.audio.play();
         this.image.style.display = "block";
