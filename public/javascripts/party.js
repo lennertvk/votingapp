2a9771dff8e7de5e4ef013d326162218bdd662a9
Joris Hens <joris@goodbytes.be>
2019-04-05 09:51:17 +0200

    Added live events with primus
    Signed-off-by: Joris Hens <joris@goodbytes.be>

--- public/javascripts/party.js
+++ public/javascripts/party.js
@@ -8,15 +8,36 @@ class Party {
         this.image = document.querySelector("#imgHorn");
         this.button = document.querySelector("#btnHorn");
 
+        // primus web sockets
+        this.primus = Primus.connect("/", {
+            reconnect: {
+                max: Infinity // Number: The max delay before we try to reconnect.
+                    ,
+                min: 500 // Number: The minimum delay before we try reconnect.
+                    ,
+                retries: 10 // Number: How many times we should try to reconnect.
+            }
+        });
+
+        this.primus.on("data", function (data) {
+            if (data.action === "playhorn") {
+                that.playHorn();
+            }
+        });
+
+
         // allow for a click on our button
         this.button.addEventListener("click", function (e) {
-            alert('clicking');
+            that.primus.write({
+                "action": "playhorn"
+            });
             e.preventDefault();
         });
     }
 
     // play the horn!
     playHorn() {
+        this.audio.volume = 0.1;
         this.audio.play();
         this.image.style.display = "block";
     }
